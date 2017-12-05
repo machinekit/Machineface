65002fce5aef5ba207911e1958f84616bd781f1d
Alexander Rössler <mail@roessler.systems>
2017-12-05 19:22:03 +0100

    updating russion translation

--- translations/machineface_ru.ts
+++ translations/machineface_ru.ts
@@ -5,237 +5,237 @@
     <name>AboutDialog</name>
     <message>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Закрыть</translation>
     </message>
     <message>
         <source>About Machineface</source>
-        <translation type="unfinished"></translation>
+        <translation>О машине</translation>
     </message>
 </context>
 <context>
     <name>ApplicationMenu</name>
     <message>
         <source>Menu</source>
-        <translation type="unfinished"></translation>
+        <translation>Меню</translation>
     </message>
     <message>
         <source>&amp;About Machineface</source>
-        <translation type="unfinished"></translation>
+        <translation>О Machineface</translation>
     </message>
     <message>
         <source>&amp;Disconnect from Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Отключиться от сессии</translation>
     </message>
     <message>
         <source>Sh&amp;utdown Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Прервать сессию</translation>
     </message>
     <message>
         <source>E&amp;xit User Interface</source>
-        <translation type="unfinished"></translation>
+        <translation>Выйти из пользовательского интерфейса</translation>
     </message>
 </context>
 <context>
     <name>ButtonJog</name>
     <message>
         <source>Move </source>
-        <translation type="unfinished"></translation>
+        <translation>Переместить</translation>
     </message>
     <message>
         <source> and </source>
-        <translation type="unfinished"></translation>
+        <translation>и</translation>
     </message>
     <message>
         <source> axis to 0</source>
-        <translation type="unfinished"></translation>
+        <translation>оси на 0</translation>
     </message>
     <message>
         <source>Select axis action</source>
-        <translation type="unfinished"></translation>
+        <translation>Выбрать действие по оси</translation>
     </message>
     <message>
         <source>Select Action</source>
-        <translation type="unfinished"></translation>
+        <translation>Выбрать действие</translation>
     </message>
     <message>
         <source>Touch off </source>
-        <translation type="unfinished"></translation>
+        <translation>Касание откл.</translation>
     </message>
     <message>
         <source> axis</source>
-        <translation type="unfinished"></translation>
+        <translation>ось</translation>
     </message>
     <message>
         <source>Change extruder</source>
-        <translation type="unfinished"></translation>
+        <translation>Заменить экструдер</translation>
     </message>
     <message>
         <source>Select extruder</source>
-        <translation type="unfinished"></translation>
+        <translation>Выбрать экструдер</translation>
     </message>
     <message>
         <source>Extruder </source>
-        <translation type="unfinished"></translation>
+        <translation>Экструдер</translation>
     </message>
     <message>
         <source>Velocity</source>
-        <translation type="unfinished"></translation>
+        <translation>Скорость</translation>
     </message>
 </context>
 <context>
     <name>DisplayPanel</name>
     <message>
         <source>Heated Bed</source>
-        <translation type="unfinished"></translation>
+        <translation>Подогрев подложки</translation>
     </message>
     <message>
         <source>Heated Chamber</source>
-        <translation type="unfinished"></translation>
+        <translation>Подогрев камеры</translation>
     </message>
     <message>
         <source>Extruder %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Экструдер %1</translation>
     </message>
     <message>
         <source>Light %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Подсветка %1</translation>
     </message>
     <message>
         <source>Fan %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Вентилятор %1</translation>
     </message>
 </context>
 <context>
     <name>GCodeTab</name>
     <message>
         <source>GCode</source>
-        <translation type="unfinished"></translation>
+        <translation>G-код</translation>
     </message>
 </context>
 <context>
     <name>GantryConfigControl</name>
     <message>
         <source>Left offset:</source>
-        <translation type="unfinished"></translation>
+        <translation>Отступ слева:</translation>
     </message>
     <message>
         <source>Right offset:</source>
-        <translation type="unfinished"></translation>
+        <translation>Отступ справа:</translation>
     </message>
 </context>
 <context>
     <name>JogControlTab</name>
     <message>
         <source>Jog</source>
-        <translation type="unfinished"></translation>
+        <translation>Толчковый режим</translation>
     </message>
 </context>
 <context>
     <name>LightControl</name>
     <message>
         <source>Pick Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Выбор цвета</translation>
     </message>
 </context>
 <context>
     <name>MdiTab</name>
     <message>
         <source>MDI</source>
-        <translation type="unfinished"></translation>
+        <translation>MDI</translation>
     </message>
 </context>
 <context>
     <name>OverlayDialog</name>
     <message>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>Ок</translation>
     </message>
 </context>
 <context>
     <name>PreviewTab</name>
     <message>
         <source>Preview</source>
-        <translation type="unfinished"></translation>
+        <translation>Предпросмотр</translation>
     </message>
 </context>
 <context>
     <name>SettingsTab</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки</translation>
     </message>
     <message>
         <source>Digital Read Out</source>
-        <translation type="unfinished"></translation>
+        <translation>Цифровое считывание</translation>
     </message>
     <message>
         <source>Show offsets</source>
-        <translation type="unfinished"></translation>
+        <translation>Показать отступы</translation>
     </message>
     <message>
         <source>Show velocity</source>
-        <translation type="unfinished"></translation>
+        <translation>Показать скорость</translation>
     </message>
     <message>
         <source>Show distance to go</source>
-        <translation type="unfinished"></translation>
+        <translation>Показать оставшееся расстояние</translation>
     </message>
     <message>
         <source>Other</source>
-        <translation type="unfinished"></translation>
+        <translation>Прочее</translation>
     </message>
     <message>
         <source>Enable preview</source>
-        <translation type="unfinished"></translation>
+        <translation>Предпросмотр</translation>
     </message>
     <message>
         <source>Maximum Velocity:</source>
-        <translation type="unfinished"></translation>
+        <translation>Максимальная скорость:</translation>
     </message>
 </context>
 <context>
     <name>TemperatureControl</name>
     <message>
         <source>N/A</source>
-        <translation type="unfinished"></translation>
+        <translation>н.д.</translation>
     </message>
 </context>
 <context>
     <name>VelocityExtrusionControl</name>
     <message>
         <source>Filament diameter:</source>
-        <translation type="unfinished"></translation>
+        <translation>Диаметр заполнителя:</translation>
     </message>
     <message>
         <source>Retract velocity:</source>
-        <translation type="unfinished"></translation>
+        <translation>Скорость вытягивания:</translation>
     </message>
     <message>
         <source>Retract length:</source>
-        <translation type="unfinished"></translation>
+        <translation>Длина вытягивания:</translation>
     </message>
     <message>
         <source>Extrude scale:</source>
-        <translation type="unfinished"></translation>
+        <translation>Масштаб экструдера:</translation>
     </message>
     <message>
         <source>Accel. adj. gain:</source>
-        <translation type="unfinished"></translation>
+        <translation>Усил. уск. настр.:</translation>
     </message>
     <message>
         <source>Velocity Extrusion</source>
-        <translation type="unfinished"></translation>
+        <translation>Скорость экструзии</translation>
     </message>
 </context>
 <context>
     <name>VideoTab</name>
     <message>
         <source>Video</source>
-        <translation type="unfinished"></translation>
+        <translation>Видео</translation>
     </message>
     <message>
         <source>Webcam not available</source>
-        <translation type="unfinished"></translation>
+        <translation>Веб-камера не обнаружена</translation>
     </message>
 </context>
 </TS>
